f7cc720d6c1d0c68ca07b2aab41dbafb57c08748
rajbopche <raj.bopche@paynearby.in>
2020-05-12 15:04:56 +0530

    feature | handled missing paths or incorrect url

--- Server.js
+++ Server.js
@@ -11,5 +11,9 @@ app.use(express.json()) //middle ware stuff, add a json parser to parse json req
 app.use('/users', UserController) //middle ware stuff, if url has /users route them to UserController
 app.use('/posts',PostController)  //middle ware stuff, if url has /users route them to PostController
 
+app.use(async (req,res) => {
+    res.status(404).json({message:"Url doesn't exist"})  // if it passes all the above valid routes(which means user entered an invalid url) we pass error message
+})
+
 db()
 app.listen(process.env.PORT_NUMBER)
